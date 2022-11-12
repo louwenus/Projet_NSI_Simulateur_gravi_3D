af7221628df7c5f48027c2b9f7fde24ddbf622d1
louwenus <louwen.fricout@gmail.com>
2022-11-12 14:27:28 +0100

    Modif of setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ Options.language_level=3
 setup(
     name='graviproject',
     version="0.0.1"
+    liscence="GPLv3"
     url="https://github.com/louwenus/Projet_NSI_Simulateur_gravi_3D"
     ext_modules=cythonize("lib/pygravilib.pyx"),
     zip_safe=False,
