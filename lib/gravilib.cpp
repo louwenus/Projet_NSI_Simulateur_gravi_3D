3051311ded0fc93e81b490f0f1622313254bc531
Kalyklos <118453265+Kalyklos@users.noreply.github.com>
2022-11-17 19:05:38 +0100

    Update gravilib.cpp

--- lib/gravilib.cpp
+++ lib/gravilib.cpp
@@ -1,5 +1,6 @@
 //  Note: Il faut penser à éditer gravilib.h,gravilib.h & gravilb.pyx 
 //  avec chaque modif des classes publiques de gravilib.cpp
+// et à ne pas faire de fautes d'ortographes
 
 #include <iostream>
 #include "gravilib.h"
