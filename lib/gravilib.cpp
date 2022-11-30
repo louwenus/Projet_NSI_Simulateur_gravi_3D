3d11ba0fdc3e72ac929eaadcf3a63386fa2d2cef
Artefact42 <signeetparaf@protonmail.com>
2022-11-30 17:09:46 +0100

    \n

--- lib/gravilib.cpp
+++ lib/gravilib.cpp
@@ -1,6 +1,6 @@
-//  Note: Il faut penser à éditer gravilib.h,gravilib.h & gravilb.pyx 
+//  Note: Il faut penser à éditer gravilib.cpp, gravilib.h & gravilb.pyx 
 //  avec chaque modif des classes publiques de gravilib.cpp
-// et à ne pas faire de fautes d'ortographes
+//  et à ne pas faire de fautes d'ortographes
 
 #include <iostream>
 #include "gravilib.h"
@@ -9,7 +9,7 @@ using std::string;
 
 
 Dimension::Dimension()
-{this->hello_text = "Hello World from cpp";}
+{this->hello_text = "Hello World from cpp\n";}
 
 void Dimension::print_hello_world() const
 {
