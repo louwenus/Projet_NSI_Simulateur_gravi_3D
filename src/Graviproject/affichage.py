5932cdb2d237e9a510ae5f22fc1df4a46dd7f7ba
Artefact42 <signeetparaf@protonmail.com>
2022-11-30 17:30:02 +0100

    première fenetre

--- src/Graviproject/affichage.py
+++ src/Graviproject/affichage.py
@@ -1,6 +1,15 @@
-print("importing affichage.py")
-try:
-    import PySide2
-except ModuleNotFoundError:
-    print("e module PySide2 devrait être installé pour que ce programme puisse fonctionner, lisez README.md pour plus de détails")
+print("Importation de affichage.py")
 
+
+try:
+    from PySide2.QtWidgets import QApplication, QLabel
+except ModuleNotFoundError:
+    print("le module PySide2 devrait être installé pour que ce programme puisse fonctionner, lisez README.md pour plus de détails")
+
+import sys
+
+
+app = QApplication(sys.argv)
+label = QLabel("<font color=red size=40>Hello World!</font>")
+label.show()
+app.exec_()
