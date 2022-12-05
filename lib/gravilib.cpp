e5211b2beabe5227de557fc4d2c4ac6c18e8aa7d
louwenus <louwen.fricout@gmail.com>
2022-12-05 17:55:42 +0100

    declared func

--- lib/gravilib.cpp
+++ lib/gravilib.cpp
@@ -6,17 +6,29 @@
 
 using std::string;
 
-
-Dimension::Dimension()
-{}
+//*******
+//Dimmension
+//*******
+Dimension::Dimension(){}
 
 void Dimension::print_hello_world() const
 {
     std::cout << "Hello World";
 }
 
+//*******
+//SimpleSphere (classe de base pour les objets)
+//*******
 SimpleSphere::SimpleSphere(llco* pos,uli* masse,uli* rayon,lco* speed)
 {this->pos = *pos;
 this->masse = *masse;
 this->rayon = *rayon;
 this->speed = *speed;}
+
+void SimpleSphere::gravite_avec(SimpleSphere* instance){ //cette function applique de la gravitation uniquement a l'instance argument (multithreading futur)
+    instance->gravite_coord(&this->pos,this->masse)
+}
+void SimpleSphere::gravite_coord(llco* pos,uli masse){}
+bool SimpleSphere::t_colision_avec(SimpleSphere* instance){}
+bool SimpleSphere::t_colision_coord(llco* pos,uli rayon){}
+bool SimpleSphere::t_coli_rapide(llco posmin,llco posmax){}
