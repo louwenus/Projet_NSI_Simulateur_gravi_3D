98b291786b55791a029fb3b0aebbb297fdea8b80
louwenus <louwen.fricout@gmail.com>
2022-12-11 18:06:55 +0100

    splited cpp in several files

---
+++ lib/typedef.hpp
@@ -0,0 +1,16 @@
+#ifndef TYPEDEF_GRAVI_CPP
+#define TYPEDEF_GRAVI_CPP
+
+#include <cmath>
+#include <string>
+#include <array>
+using std::string;
+
+typedef unsigned long long int ulli;
+typedef long long int lli;
+typedef long int li;
+typedef unsigned long int uli;
+typedef std::array<lli, 3> llco;
+typedef std::array<li, 3> lco;
+
+#endif 
