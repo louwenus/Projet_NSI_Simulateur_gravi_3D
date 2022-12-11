64280cef737bc01e49987c1e5d9aceee87cd9474
louwenus <louwen.fricout@gmail.com>
2022-12-11 22:08:42 +0100

    Dummy sphere

--- lib/sphere.cpp
+++ lib/sphere.cpp
@@ -5,28 +5,35 @@ using std::string;
 //*******
 //SimpleSphere (classe de base pour les objets)
 //*******
-SimpleSphere::SimpleSphere(llco* pos,uli masse,uli rayon,lco* speed)
-{this->pos = *pos;
+SimpleSphere::SimpleSphere(llco &pos,uli masse,uli rayon,lco &speed)
+{this->pos = pos;
 this->masse = masse;
 this->rayon = rayon;
-this->speed = *speed;}
-
-void SimpleSphere::gravite_avec(SimpleSphere &instance,const float temps)const{ //cette function applique de la gravitation uniquement a l'instance argument (multithreading futur)
+this->speed = speed;
+}
+void SimpleSphere::move(float temps){
+    this->pos[0]+=this->speed[0]*temps;
+    this->pos[1]+=this->speed[1]*temps;
+    this->pos[2]+=this->speed[2]*temps;
+}
+//gravitation
+void SimpleSphere::gravite_avec(DummySphere &instance,float temps)const{ //cette function applique de la gravitation uniquement a l'instance argument (multithreading futur)
     instance.gravite_coord(this->pos,this->masse,temps);
 }
-void SimpleSphere::gravite_coord(const llco &pos,const uli masse,const float temps){
+void SimpleSphere::gravite_coord(const llco &pos,const uli masse,float temps){
     llco dif={pos[0]-this->pos[0],pos[1]-this->pos[1],pos[2]-this->pos[2]};  //diff pos par pos
     ulli divide=(abs(dif[0])+pow(abs(dif[0]),2)+abs(dif[1])+pow(abs(dif[1]),2)+abs(dif[2])+pow(abs(dif[2]),2))/temps;  //diviseurs = dist^2 + sum (dif) / temps(on remultiplie par les composantes de diff)
     this->speed[0]+=dif[0]/divide;
     this->speed[1]+=dif[1]/divide;
     this->speed[2]+=dif[2]/divide;
 }
-bool SimpleSphere::t_collision_avec(SimpleSphere &instance){
+//collision
+bool SimpleSphere::t_collision_avec(DummySphere &instance){
     if (not instance.t_colli_rapide(this->posmin,this->posmax))
     {return false;}
     if (instance.t_collision_coord(this->pos,this->rayon)){}
 }
-bool SimpleSphere::t_collision_coord(const llco &pos,const uli rayon)const{
+bool SimpleSphere::t_collision_coord(const llco &pos,uli rayon)const{
     if (pow(pos[0]-this->pos[0],2)+pow(pos[0]-this->pos[0],2)+pow(pos[0]-this->pos[0],2)
     <pow(rayon+this->rayon,2)){return true;}
     return false;
@@ -38,8 +45,9 @@ bool SimpleSphere::t_colli_rapide(const llco &posmin,const llco &posmax)const{
 //*******
 // Dummy sphere
 //*******
-void DummySphere::gravite_avec(SimpleSphere &instance,const float temps)const{return;}
-void DummySphere::gravite_coord(const llco &pos,const uli masse,const float temps){return;}
-bool DummySphere::t_collision_avec(SimpleSphere &instance){return false;}
+void DummySphere::move(float temps){return;}
+void DummySphere::gravite_avec(DummySphere &instance,float temps)const{return;}
+void DummySphere::gravite_coord(const llco &pos,uli masse,float temps){return;}
+bool DummySphere::t_collision_avec(DummySphere &instance){return false;}
 bool DummySphere::t_collision_coord(const llco &pos,const uli rayon)const{return false;}
 bool DummySphere::t_colli_rapide(const llco &posmin,const llco &posmax)const{return false;}
--- lib/sphere.hpp
+++ lib/sphere.hpp
@@ -3,18 +3,19 @@
 #include "typedef.hpp"
 class SimpleSphere : public DummySphere{
 public:
-    //constructeurs
+    //constructeurs et destructeur
     SimpleSphere();
-    SimpleSphere(llco* pos,uli masse,uli rayon,lco* speed);
+    SimpleSphere(llco &pos,uli masse,uli rayon,lco &speed);
 
-    virtual void gravite_avec(SimpleSphere &instance,const float temps) const;
-    virtual void gravite_coord(const llco &pos,const uli masse,const float temps);
-    virtual bool t_collision_avec(SimpleSphere &instance);
+    virtual void move(float temps);
+    virtual void gravite_avec(DummySphere &instance,float temps) const;
+    virtual void gravite_coord(const llco &pos,uli masse,float temps);
+    virtual bool t_collision_avec(DummySphere &instance);
     virtual bool t_collision_coord(const llco &pos,uli rayon) const;
     virtual bool t_colli_rapide(const llco &posmin,const llco &posmax) const;
 protected:
-    llco posmin;
-    llco posmax;
+    llco posmin;  //utilisé pour les tests de collision rapide
+    llco posmax;  //utilisé pour les tests de collision rapide
     llco pos;
     uli masse;
     uli rayon;
@@ -26,10 +27,11 @@ class DummySphere{ //may be used for pending deletion sphere for exemple, and as
 public:
     //constructeurs
     DummySphere();
-
-    virtual void gravite_avec(SimpleSphere &instance,const float temps) const;
-    virtual void gravite_coord(const llco &pos,const uli masse,const float temps);
-    virtual bool t_collision_avec(SimpleSphere &instance);
+    
+    virtual void move(float temps);
+    virtual void gravite_avec(DummySphere &instance,float temps) const;
+    virtual void gravite_coord(const llco &pos,uli masse,float temps);
+    virtual bool t_collision_avec(DummySphere &instance);
     virtual bool t_collision_coord(const llco &pos,uli rayon) const;
     virtual bool t_colli_rapide(const llco &posmin,const llco &posmax) const;
     
