ab4816687552f64063e033c72a44b493241e9bf4
louwenus <louwen.fricout@gmail.com>
2023-03-24 22:02:23 +0100

    settings work

--- src/Graviproject/settings.py
+++ src/Graviproject/settings.py
@@ -1,25 +1,24 @@
 import json
 from typing import Iterable
 
-settings={}
-defaults={}
-print("initializing settings")
+settings: dict={}
+defaults: dict={}
 try:
     with open('default_settings.json','r') as setfile:
         defaults=json.load(setfile)
 except Exception as e:
-    print("no default_settings.json file, aborting")
-    raise
+    print("no default_settings.json file, package should be réinstalled or permission checked, aborting")
+    raise FileNotFoundError('default_settings.json')
 try:
     with open('settings.json','r') as setfile:
         settings=json.load(setfile)
 except:
-    print("inexistant or ill-formated settings.json file, using defaults settings")
+    print("inexistant or ill-formated settings.json file, using defaults settings only")
 
-def access(setloc : Iterable[str]):
+def get(setloc : str):
     try:
         temp=settings
-        for key in setloc:
+        for key in setloc.split('.'):
             temp=temp[key]
         return temp
     except KeyError:
@@ -32,30 +31,30 @@ def access(setloc : Iterable[str]):
             print("No setting found for ",setloc)
             return None
 
-def set_setting(setloc : Iterable[str],value) -> bool:
+def set(setloc : str,value) -> bool:
     """set settings 'setloc' at value
 
     Args:
-        setloc (Iterable[str]): iterable of the setting path (eg ("setting_group","seting_name"))
+        setloc (str): setting to update, a . permit to separate per group, ..is not permited
         value (any): the new value of the setting
 
     Returns:
         bool: _description_
     """
     temp=settings
-    for key in setloc[:-1]:
+    for key in setloc.split('.')[:-1]:
         if type(temp) is not dict:
             print("setting path to non dict, abborting")
             return False
         if key not in temp:
             temp[key]={}
         temp=temp[key]
-    temp[setloc[-1]]=value
+    temp[setloc.split('.')[-1]]=value
     return True
 
 
 
-def save_settings()->bool:
+def save()->bool:
     """save settings to settings.json
 
     Returns:
@@ -68,3 +67,7 @@ def save_settings()->bool:
     except Exception as e:
         print("Illegal setting for json representation or no write acess:",e)
         return False
+
+
+if get("logging") >=2:
+    print("INFO: settings initialized")
