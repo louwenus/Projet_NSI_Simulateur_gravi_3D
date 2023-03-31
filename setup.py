c742bb4e5fb0d57fd8b583b9d750a11e0905fd88
louwenus <louwen.fricout@gmail.com>
2023-03-31 12:49:52 +0200

    -lttb

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ import os
 
 
 Options.language_level=3
-copt: dict[str, list[str]] =  {#'unix': ['-std=c++20','-g','-Og','-pthread','-ffast-math']  ,
-        'unix': ['-std=c++20','-O3','-pthread','-ffast-math','-ftree-parallelize-loops=10']  ,
+copt: dict[str, list[str]] =  {#'unix': ['-std=c++20','-g','-Og','-pthread','-ffast-math','-lttb']  ,
+        'unix': ['-std=c++20','-O3','-pthread','-ffast-math','-ftree-parallelize-loops=10','-lttb']  ,
         'mingw32' : ['-std=c++20','-O3','-pthread','-ffast-math']  ,
         'msvc'  : ['/O2','/std:c++20','/cgthreads8']  , 
         #'cygiwin' : []
