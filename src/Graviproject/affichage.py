e890dfbb3fcc2dfc9179ef14fe2bd70e220a10f1
Kalyklos <matthebg73@gmail.com>
2023-04-14 14:46:25 +0200

    faut reglee la profondeur

--- src/Graviproject/affichage.py
+++ src/Graviproject/affichage.py
@@ -226,8 +226,8 @@ class Controles(QWidget):
             dist:float=dist/(abs(x)+abs(y)+abs(z))
             x=xmean+sqrt(x*dist)*xrand*100000
             y=ymean+sqrt(y*dist)*yrand*100000
-            z=zmean+sqrt(z*dist)*zrand*1000
-            var = gravilib.PyBaseSphere(x, y, z, randint(1,1000), randint(30000000, 40000000), randint(-4000, 4000), randint(-4000, 4000), randint(-30, 30), randint(1,15))
+            z=zmean+sqrt(z*dist)*zrand*100
+            var = gravilib.PyBaseSphere(x, y, z, randint(1,1000), randint(30000000, 40000000), randint(-4000, 4000), randint(-4000, 4000), randint(-3, 30), randint(1,15))
             Fenetre_principale.ajouter_sphere(var)
 
     bouton_val_aj: QAbstractButton = QPushButton("Ajouter les sphères")
