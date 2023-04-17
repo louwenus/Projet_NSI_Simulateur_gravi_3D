3facb9884cd4150c139f55c1e45cb54201701c26
Bjctrhtg <alexis.dallot@orange.fr>
2023-04-17 14:09:12 +0200

    correction round --> int de l'absorbtion

--- src/Graviproject/gravilib.py
+++ src/Graviproject/gravilib.py
@@ -126,14 +126,14 @@ def absorption (sphere1:PyBaseSphere, sphere2:PyBaseSphere):
     volume +=(sphere2.get_rayon()**3)
     rayon =int(volume**(1/3))
     sphere1.set_rayon(rayon)
-    
-    masse_final: int=sphere1.get_masse()
-    masse_final +=sphere2.get_masse()
+    m1 = sphere1.get_masse()
+    m2 = sphere2.get_masse()
+    masse_final: int= m1+m2
     vx1,vy1,vz1=sphere1.get_speed()
     vx2,vy2,vz2=sphere2.get_speed()
-    vitessex = round(sqrt((sphere1.get_masse()*vx1**2 + sphere2.get_masse()*vx2**2)/masse_final), 0)    #gain (ou perte) de vitesse
-    vitessey = round(sqrt((sphere1.get_masse()*vy1**2 + sphere2.get_masse()*vy2**2)/masse_final), 0)    #selon la formule energie cinétique
-    vitessez = round(sqrt((sphere1.get_masse()*vz1**2 + sphere2.get_masse()*vz2**2)/masse_final), 0)    #=masse*vitesse**2.
+    vitessex = int(sqrt((m1*vx1**2 + m2*vx2**2)/masse_final), 0)    #gain (ou perte) de vitesse
+    vitessey = int(sqrt((m1*vy1**2 + m2*vy2**2)/masse_final), 0)    #selon la formule energie cinétique
+    vitessez = int(sqrt((m1*vz1**2 + m2*vz2**2)/masse_final), 0)    #=masse*vitesse**2.
     sphere1.set_masse(masse_final)
     sphere1.set_speed(vitessex, vitessey, vitessez)
     
